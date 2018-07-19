347f1e31eb620bfe4a24386161e6b3791d6a559b
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 16:25:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.length == 2 
     return array.join("and")
   elsif array.length == 1 
-    return array.split
+    return array.join
   end
   
     array.each_with_index do |fruit, i|
