518492f47916ee06f595443191bcf4109601e586
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 16:28:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  str = ""
+  if array.length == 2 
+    return array.join(" and ")
+  elsif array.length == 1 
+    return array.join
+  end
+  
+    array.each do |fruit|
+      if fruit == array.last
+        str << "and #{fruit}"
+      else 
+        str << "#{fruit}, "
+      end
+    end
+    return str 
 end
